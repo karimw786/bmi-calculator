7a847f6f0f8d8a8c0beaf1d633e41e00e2aa7dec
Karim Wallani <karimw786@hotmail.com>
2025-01-12 23:07:21 -0500

    Reduce the number of onChange handlers to one

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -14,17 +14,21 @@ export default function UserInput({ passBmiToParent }: UserInputProps) {
     passBmiToParent(isNaN(calculatedBmi) ? 'Invalid input.' : calculatedBmi);
   }, [heightFeet, heightInches, weightPounds]);
 
-  const handleHeightFeetChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setHeightFeet(event.target.value);
-  };
-
-  const handleHeightInchesChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setHeightInches(event.target.value);
-  };
-
-  const handleWeightPoundsChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setWeightPounds(event.target.value);
-  };  
+  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    switch (event.target.name) {
+      case "heightFeet":
+        setHeightFeet(event.target.value);
+        break;
+      case "heightInches":
+        setHeightInches(event.target.value);  
+        break;
+      case "weightPounds":
+        setWeightPounds(event.target.value);  
+        break;    
+      default:
+        break;
+    }
+  }
 
   return (
     <div id="input-container" className="w-[290px] border-2 border-gray-100 mx-auto bg-white shadow-lg rounded-lg p-4 mt-3">
@@ -32,11 +36,11 @@ export default function UserInput({ passBmiToParent }: UserInputProps) {
         <h2 className="text-gray-500 font-bold mt-1 mb-1">Enter Your Height:</h2>
         <div className="flex flex-row gap-2">
           <div className="flex flex-col w-[125px]">
-            <input type="text" className="border-2 border-slate-200 p-1 text-center" name="heightFeet" value={heightFeet} onChange={handleHeightFeetChange} />
+            <input type="text" className="border-2 border-slate-200 p-1 text-center" name="heightFeet" value={heightFeet} onChange={handleInputChange} />
             <label htmlFor="heightFeet" className="p-1 text-center">Feet</label>
           </div>
           <div className="flex flex-col w-[125px]">
-            <input type="text" className="border-2 border-slate-200 p-1 text-center" name="heightInches" value={heightInches} onChange={handleHeightInchesChange} />
+            <input type="text" className="border-2 border-slate-200 p-1 text-center" name="heightInches" value={heightInches} onChange={handleInputChange} />
             <label htmlFor="heightInches" className="p-1 text-center">Inches</label>
           </div>
         </div>
@@ -44,7 +48,7 @@ export default function UserInput({ passBmiToParent }: UserInputProps) {
       <div id="weight-input">
         <h2 className="text-gray-500 font-bold mt-1 mb-1">Enter Your Weight:</h2>
         <div className="flex flex-col w-[125px]">
-          <input type="text" className="border-2 border-slate-200 p-1 text-center" name="weightPounds" value={weightPounds} onChange={handleWeightPoundsChange} />
+          <input type="text" className="border-2 border-slate-200 p-1 text-center" name="weightPounds" value={weightPounds} onChange={handleInputChange} />
           <label htmlFor="weightPounds" className="p-1 text-center">Pounds</label>
         </div>
       </div>
